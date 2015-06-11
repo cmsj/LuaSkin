71cac73c55faa59c920bcd62bfb32c48d10b8118
Chris Jones <cmsj@tenshu.net>
2015-06-11 15:09:11 +0100

    Initial Commit

---
+++ LuaSkin/LuaSkin.h
@@ -0,0 +1,19 @@
+//
+//  LuaSkin.h
+//  LuaSkin
+//
+//  Created by Chris Jones on 11/06/2015.
+//  Copyright (c) 2015 Hammerspoon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LuaSkin.
+FOUNDATION_EXPORT double LuaSkinVersionNumber;
+
+//! Project version string for LuaSkin.
+FOUNDATION_EXPORT const unsigned char LuaSkinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LuaSkin/PublicHeader.h>
+
+
