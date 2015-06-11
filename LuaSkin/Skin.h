8108b9766bbd85909e4c275b2e005b4b0664cc55
Chris Jones <cmsj@tenshu.net>
2015-06-11 20:30:09 +0100

    Move docstrings to Skin.h so they show up in external places

--- LuaSkin/Skin.h
+++ LuaSkin/Skin.h
@@ -19,6 +19,29 @@
 
 - (void)createLuaState;
 - (void)destroyLuaState;
+
+/** Calls lua_pcall() with debug.traceback() as the message handler
+ 
+ @code
+ // First push a reference to the Lua function you want to call
+ lua_rawgeti(L, LUA_REGISTRYINDEX, fnRef);
+ 
+ // Then push the parameters for that function, in order
+ lua_pushnumber(L, 1);
+ 
+ // Finally, call protectedCallAndTraceback, telling it how
+ // many arguments you pushed, and how many results you expect
+ BOOL result = [luaSkin protectedCallAndTraceback:1 nresults:0];
+ 
+ // The boolean return tells you whether the Lua code threw
+ // an exception or not
+ if (!result) handleSomeError();
+ @endcode
+ 
+ @return NO if the Lua code threw an exception, otherwise YES
+ @param nargs An integer specifying how many function arguments you have pushed onto the stack
+ @param nresults An integer specifying how many return values the Lua function will push onto the stack
+ */
 - (BOOL)protectedCallAndTraceback:(int)nargs nresults:(int)nresults;
 
 @end
--- LuaSkin/Skin.m
+++ LuaSkin/Skin.m
@@ -40,28 +40,6 @@
 
 #pragma mark - Methods for calling into Lua from C
 
-/** Calls lua_pcall() with debug.traceback() as the message handler
- 
- @code
- // First push a reference to the Lua function you want to call
- lua_rawgeti(L, LUA_REGISTRYINDEX, fnRef);
- 
- // Then push the parameters for that function, in order
- lua_pushnumber(L, 1);
- 
- // Finally, call protectedCallAndTraceback, telling it how 
- // many arguments you pushed, and how many results you expect
- BOOL result = [luaSkin protectedCallAndTraceback:1 nresults:0];
- 
- // The boolean return tells you whether the Lua code threw
- // an exception or not
- if (!result) handleSomeError();
- @endcode
- 
- @return NO if the Lua code threw an exception, otherwise YES
- @param nargs An integer specifying how many function arguments you have pushed onto the stack
- @param nresults An integer specifying how many return values the Lua function will push onto the stack
- */
 - (BOOL)protectedCallAndTraceback:(int)nargs nresults:(int)nresults {
     // At this point we are being called with nargs+1 items on the stack, but we need to shove our traceback handler below that
     
